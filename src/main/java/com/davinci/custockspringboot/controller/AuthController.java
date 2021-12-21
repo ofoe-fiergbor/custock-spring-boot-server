6529aa42c2b504b976fd34e445477a5bb86643c1
Ofoe Fiergbor <ofoefiergbor16@gmail.com>
2021-12-21 01:37:37 +0000

    cross origin

--- src/main/java/com/davinci/custockspringboot/controller/AuthController.java
+++ src/main/java/com/davinci/custockspringboot/controller/AuthController.java
@@ -9,13 +9,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("api/v1/auth")
 @AllArgsConstructor
